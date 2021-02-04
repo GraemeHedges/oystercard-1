15a6c674b40c9670db50ecbf785a1e974c7a1fde
Graemehedges <graeme.hedges@gmail.com>
2021-02-04 11:45:08 +0000

    added journey history, tested and refactored - touch_out method does too much work

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,12 @@
 class Oystercard
   LIMIT = 90
   MINIMUM = 1
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :journey_history, :current_journey
   def initialize(balance = 0)
     @balance = balance
     @entry_station = nil
+    @journey_history = []
+    @current_journey = {entry_station: nil, exit_station: nil}
   end
 
   def top_up(amount)
@@ -15,15 +17,19 @@ class Oystercard
   def touch_in(station)
     raise 'Insufficient funds for journey' if @balance < MINIMUM
     @entry_station = station
+    @current_journey[:entry_station] = entry_station
   end
 
-  def touch_out
+  def touch_out(exit_station)
     deduct(MINIMUM)
     @entry_station = nil
+    @current_journey[:exit_station] = exit_station
+    @journey_history << @current_journey
+    @current_journey = {entry_station: nil, exit_station: nil}
   end
 
   def in_journey?
-    @entry_station
+    @current_journey[:entry_station]
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -27,14 +27,14 @@ describe Oystercard do
 
   describe '#deduct' do
     it 'deducts money from balance when customer travels' do
-      subject.touch_out
+      subject.touch_out("Waterloo")
       expect(subject.balance).to eq $limit - $minimum
     end
   end
 
   describe '#touch_in' do
     it 'does not allow a customer to touch in when the balance is below the minimum' do
-      subject = Oystercard.new # this overrides the before top up to set balance to 0, better way to do this?
+      subject = Oystercard.new
       expect{subject.touch_in("Victoria")}.to raise_error 'Insufficient funds for journey'
     end
 
@@ -48,12 +48,19 @@ describe Oystercard do
 
   describe '#touch_out' do
     it 'deducts the minimum amount from the balance when touched out' do
-      expect{subject.touch_out}.to change{subject.balance}.by -$minimum
+      expect{subject.touch_out("Waterloo")}.to change{subject.balance}.by -$minimum
     end
 
     it 'forgets entry_station when journey ends' do
       subject.touch_in("Victoria")
-      expect{ subject.touch_out }.to change{subject.entry_station}.to nil
+      expect{ subject.touch_out("Waterloo") }.to change{subject.entry_station}.to nil
+    end
+
+    it 'takes an exit station as an argument' do
+      station = double
+      allow(station).to receive(:name) { "Waterloo"}
+      subject.touch_out(station.name)
+      expect(subject.journey_history[0][:exit_station]).to eq station.name
     end
   end
 
@@ -61,10 +68,6 @@ describe Oystercard do
     it 'expect a new cards initial state not to be in a journey' do
       expect(subject).not_to be_in_journey
     end
-
-    # not sure if this is better nested or unnested?
-    # the idea is to DRY out the touch ins from the
-    # tests so maybe the nest hurts readability
 
     describe '#in_journey after a card is touched in' do      
       before(:each) do
@@ -76,9 +79,42 @@ describe Oystercard do
       end
   
      it 'still works after card #touch_in then #touch_out' do
-        subject.touch_out
+        subject.touch_out("Waterloo")
         expect(subject).not_to be_in_journey
       end      
     end
   end
+
+  describe 'journey_history' do
+    it 'expects a card to have no journy history when it is new' do
+      expect(subject.journey_history).to be_empty
+    end
+
+    it 'expects touching in and out to add an entry and exit station to journey history' do
+      subject.touch_in("Victoria")
+      subject.touch_out("Waterloo")
+      expect(subject.journey_history).to eq [{entry_station: "Victoria", exit_station: "Waterloo"}]
+    end
+  end
 end
+
+
+# context blocks
+
+# new card
+# has no balance
+# has no journey history
+# can be topped up
+# can be touched in
+# can be touched out
+
+# card in a journey
+# set up - has a a balance, has an entry station
+# 
+# can be touched out
+
+# it tests:
+# 'expects touching in and out to add an entry and exit station to journey history'
+# 'expects a card to have no journy history when it is new'
+# 'still works after card #touch_in then #touch_out'
+# 'returns whether a card is in use after #touch_in'
